5d6c344acd758e16f12a8fb2d12b3ab9494e9282
udit kumar <uditkumar489@gmail.com>
2018-01-02 07:12:08 +0530

    initial commit
    necessary imports done !1

---
+++ inspector.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  2 07:09:34 2018
+
+@author: Udit
+"""
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
